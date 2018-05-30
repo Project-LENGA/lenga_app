ed1a98eaab40533c4a6496f0fc98afb421bfe5f1
EC2 Default User <ec2-user@ip-172-31-11-4.us-east-2.compute.internal>
2018-05-30 01:58:16 +0000

    Fill in layout of the top page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Return a proper title for each page
+  def full_title(page_title = '')
+    base_title = "Lenga"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/security'
-
-  get 'static_pages/sitemap'
-
-  get 'static_pages/become_a_tutor'
-
-  get 'static_pages/tips'
-
-  get 'static_pages/terms'
-
-  get 'static_pages/create_an_account'
-
+  get  '/help',              to: 'static_pages#help'
+  get  '/about',             to: 'static_pages#about'
+  get  '/security',          to: 'static_pages#security'
+  get  '/sitemap',           to: 'static_pages#sitemap'
+  get  '/become_a_tutor',    to: 'static_pages#become_a_tutor'
+  get  '/tips',              to: 'static_pages#tips'
+  get  '/terms',             to: 'static_pages#terms'
+  get  '/create_an_account', to: 'static_pages#create_an_account'
 end
