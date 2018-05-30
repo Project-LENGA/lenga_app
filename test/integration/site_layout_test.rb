02a0e0eccdfaf76c62a728faf0895bb7e63ea035
EC2 Default User <ec2-user@ip-172-31-11-4.us-east-2.compute.internal>
2018-05-30 21:54:21 +0000

    Add Integration test for layout

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path, count: 3
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", security_path
+    assert_select "a[href=?]", sitemap_path
+    assert_select "a[href=?]", become_a_tutor_path
+    assert_select "a[href=?]", tips_path
+    assert_select "a[href=?]", terms_path
+    assert_select "a[href=?]", create_an_account_path
+  end
+end
