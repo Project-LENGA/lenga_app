3bf2b273c73ebcaca33f3b14fff92914e48a64ce
eiji03aero <eiji03aero@gmail.com>
2018-06-16 23:03:38 +0900

    fin merged

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,16 @@ ActiveRecord::Schema.define(version: 20180614073254) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.text "content"
+    t.string "title"
+  end
+
+  create_table "tutors", force: :cascade do |t|
+    t.date "graduate_year"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tutors_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
