367d9e5dfe4788c6aedf560a97d6251068e0aa1c
EC2 Default User <ec2-user@ip-172-31-11-4.us-east-2.compute.internal>
2018-06-22 08:03:48 +0000

    Make the student model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180614073254) do
+ActiveRecord::Schema.define(version: 20180622065521) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "content"
     t.string "title"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_students_on_user_id"
   end
 
   create_table "tutors", force: :cascade do |t|
